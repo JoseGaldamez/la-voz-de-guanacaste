683827bba8ece1928e1ee40f97533439e8d31aae
Jose Galdamez <josegaldamez1991@gmail.com>
2022-09-11 12:14:17 -0600

    removing firebaseConfig

---
+++ README.md
@@ -0,0 +1,1 @@
+## La Voz de Guanacaste App
--- dashboard/dashboard-lavozdeguanacaste/src/auth/firebaseAuth.js
+++ dashboard/dashboard-lavozdeguanacaste/src/auth/firebaseAuth.js
@@ -2,17 +2,8 @@ import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 import { getAuth } from "firebase/auth";
 import { getStorage } from "firebase/storage";
+import { firebaseConfig } from "./firebaseConfig";
 
-export const firebaseConfig = {
-  apiKey: "AIzaSyCvE0_BSbFh0MvPqF4KpWrM5fG5FYmNWNU",
-  authDomain: "lavozdeguanacaste-app.firebaseapp.com",
-  databaseURL: "https://lavozdeguanacaste-app-default-rtdb.firebaseio.com",
-  projectId: "lavozdeguanacaste-app",
-  storageBucket: "lavozdeguanacaste-app.appspot.com",
-  messagingSenderId: "670473558994",
-  appId: "1:670473558994:web:03fc9dd327a5d882e3c60b",
-  measurementId: "G-HD7BVEGEE9",
-};
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
 const auth = getAuth(app);
